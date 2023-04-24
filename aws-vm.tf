878b4e1e01932969ab4388d584cf840bec8cdf3d
lotusanirudh <103250606+lotusanirudh@users.noreply.github.com>
2023-04-24 18:38:47 +0000

    Update aws-vm.tf

--- aws-vm.tf
+++ aws-vm.tf
@@ -3,7 +3,7 @@ provider "aws" {
 
 }
 resource "aws_instance" "codebuildvm" {
-  instance_type        = "t2.large"
+  instance_type        = "t2.micro"
   ami                  = "ami-06c2ec1ceac22e8d6"
   tags = {
     Name          = "sentinel-check"
